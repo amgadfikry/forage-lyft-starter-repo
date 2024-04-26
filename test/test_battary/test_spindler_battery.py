be8aaad9870dba8d31b3080cb4b1bedffe2e7590
amgadfikry <dr.amagd_sh92@yahoo.com>
2024-04-26 22:54:22 +0300

    change test to suitable for new changed features

--- test/test_battary/test_spindler_battery.py
+++ test/test_battary/test_spindler_battery.py
@@ -4,7 +4,7 @@ from battary.spindler_battery import SpindlerBattary
 
 class TestSpindlerBattery(unittest.TestCase):
   def test_true_need_service(self):
-    current_date = datetime.date(2020, 1, 1)
+    current_date = datetime.date(2020, 1, 2)
     last_service_date = datetime.date(2017, 1, 1)
     spindler_battery = SpindlerBattary(current_date, last_service_date)
     self.assertTrue(spindler_battery.need_service())
