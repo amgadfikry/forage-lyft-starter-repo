b3f12a6634070e3cdadb8d7f7820891fa8513a4b
amgadfikry <dr.amagd_sh92@yahoo.com>
2024-04-26 22:50:43 +0300

    add tires to class of car and check it in need service method

--- car.py
+++ car.py
@@ -9,9 +9,10 @@ This is a simple class that represents a car. It has an engine and a battery.
     needs_service: Returns True if either the engine or the battery needs service.
 """
 class Car(Serviceable):
-    def __init__(self, engine, battery):
+    def __init__(self, engine, battery, tires):
         self.engine = engine
         self.Battery = battery
+        self.tires = tires
 
     def needs_service(self):
-        return self.engine.needs_service() or self.battery.needs_service()
+        return self.engine.needs_service() or self.battery.needs_service() or self.tires.needs_service()
